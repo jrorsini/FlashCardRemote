f45b3090b71baf48baae6f739459dd9c815928b0
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2026-02-10 16:04:53 +0900

    added Flashcards typed questions

---
+++ data/questions.ts
@@ -0,0 +1,19 @@
+import { FlashCard } from '@/types';
+
+export const questions: FlashCard[] = [
+    {
+        id: 1,
+        question: 'what is called a Hindi Full Stop ?',
+        answer: 'puurnaviraam',
+    },
+    {
+        id: 2,
+        question: 'how many vowels has hindi',
+        answer: '11',
+    },
+    {
+        id: 3,
+        question: 'how many consonants has hindi?',
+        answer: '33',
+    },
+];
