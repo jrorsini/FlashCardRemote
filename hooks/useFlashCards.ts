b7a1936fab3d44dff6005fe0b718bc9dbd591825
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2026-02-13 23:19:08 +0900

    feat(hook): added logic for card status.

--- hooks/useFlashCards.ts
+++ hooks/useFlashCards.ts
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-import { FlashCard } from '@/types';
+import { FlashCard, CardStatus } from '@/types';
+
+type CardStatuses = Record<number, CardStatus>;
 
 export function useFlashCards(cards: FlashCard[]) {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const [isFlipped, setIsFlipped] = useState(false);
+
+    const [cardStatuses, setCardStatuses] = useState<CardStatuses>({});
 
     const goToNext = () => {
         if (currentIndex < cards.length - 1) {
@@ -29,6 +33,31 @@ export function useFlashCards(cards: FlashCard[]) {
         setIsFlipped(false);
     };
 
+    const markCard = (status: CardStatus) => {
+        setCardStatuses((prev) => ({
+            ...prev,
+            [cards[currentIndex].id]: status,
+        }));
+
+        goToNext();
+    };
+
+    const getCurrentStatus = (): CardStatus => {
+        return cardStatuses[cards[currentIndex].id] ?? 'Unseen';
+    };
+
+    const getStats = () => {
+        const known = Object.values(cardStatuses).filter(
+            (s) => s === 'known',
+        ).length;
+        const review = Object.values(cardStatuses).filter(
+            (s) => s === 'review',
+        ).length;
+        const unseen = cards.length - known - review;
+
+        return { known, review, unseen };
+    };
+
     return {
         currentCard: cards[currentIndex],
         currentIndex,
@@ -40,5 +69,8 @@ export function useFlashCards(cards: FlashCard[]) {
         reset,
         isFirst: currentIndex === 0,
         isLast: currentIndex === cards.length - 1,
+        markCard,
+        currenStatus: getCurrentStatus(),
+        stats: getStats(),
     };
 }
